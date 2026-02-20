4802326d7a72a579f1f7c4f5454e537be245e211
atom <omidrazzaghi2000@gmail.com>
2026-02-20 13:45:18 +0330

    change: change ip address in config

--- new_frontend/config/api.js
+++ new_frontend/config/api.js
@@ -1,8 +1,8 @@
 // API configuration
 // import.meta.env.VITE_REACT_APP_API_URL
-const API_BASE_URL = import.meta.env.VITE_REACT_APP_API_URL || 'http://localhost:8000';
+const API_BASE_URL = import.meta.env.VITE_REACT_APP_API_URL || 'http://62.60.214.42:8000';
 
-export const API_URL = API_BASE_URL || (import.meta.env.NODE_ENV === 'production' ? '' : 'http://localhost:8000');
+export const API_URL = API_BASE_URL || (import.meta.env.NODE_ENV === 'production' ? '' : 'http://62.60.214.42:8000');
 
 export default API_URL;
 
